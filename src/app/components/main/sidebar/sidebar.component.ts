65e87f67141f6cca148e28ea87ec434d9e1f9f6c
nurmoohamedi <nurmoohamedi@gmail.com>
2023-04-04 10:10:53 +0600

    FIX: UI/UX правки по sidebar

--- src/app/components/main/sidebar/sidebar.component.ts
+++ src/app/components/main/sidebar/sidebar.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
   public navList: any = [
-    { id: 1, name: 'Home', router: '', icon: 'home-icon' },
-    { id: 2, name: 'Playlist', router: 'playlists', icon: 'playlists' },
-    { id: 3, name: 'Musics', router: 'musics', icon: 'musics' }
+    { id: 1, name: 'Home', router: '', icon: 'nav_music.svg' },
+    { id: 2, name: 'Playlist', router: 'playlists', icon: 'nav_playlist.svg' },
+    { id: 3, name: 'Musics', router: 'musics', icon: 'nav_liked.svg' }
   ]
 }
